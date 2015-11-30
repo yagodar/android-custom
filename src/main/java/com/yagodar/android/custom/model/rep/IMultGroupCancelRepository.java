aaec71a3e6f945e05c9d1b98dd1d2cca9b36b718
yagodar <yagodarov.a.e@gmail.com>
2015-11-30 23:54:26 +0300

    Working on cancel repository

---
+++ src/main/java/com/yagodar/android/custom/model/rep/IMultGroupCancelRepository.java
@@ -0,0 +1,11 @@
+package com.yagodar.android.custom.model.rep;
+
+import com.yagodar.essential.model.Model;
+import com.yagodar.essential.model.rep.IMultGroupRepository;
+
+/**
+ * Created by yagodar on 30.11.2015.
+ */
+public interface IMultGroupCancelRepository<T extends Model> extends IMultGroupRepository {
+    //TODO
+}
